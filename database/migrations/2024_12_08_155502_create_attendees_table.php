d5fc85d51667de80b230d539d9cc2147b5c50bc7
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2024-12-08 23:56:06 +0800

    database/migrations/2024_12_08_155343_create_events_table.phpdatabase/migrations/2024_12_08_155343_create_events_table.php

---
+++ database/migrations/2024_12_08_155502_create_attendees_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id')->constrained('events');
+            $table->string('name');
+            $table->string('email');
+            $table->enum('rsvp_status', ['attending', 'not_attending']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendees');
+    }
+};
